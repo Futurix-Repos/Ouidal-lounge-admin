20d49236c89f9767f2a4603675ffd622bd174c41
Eddy <63247362+Maestro2496@users.noreply.github.com>
2023-05-30 03:29:34 +0100

    Bundle products

---
+++ components/modals/update-bundle-product.tsx
@@ -0,0 +1,272 @@
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Field, FieldArray, Form, Formik } from "formik";
+import { TrashIcon } from "@heroicons/react/20/solid";
+import { useMutation, useQuery } from "react-query";
+import { fetcher } from "@/helpers";
+import axios from "axios";
+import { queryClient } from "@/pages/_app";
+
+export default function UpdateBundleProduct({ open, setOpen, product }) {
+  const { data: ingredients, isLoading } = useQuery({
+    queryKey: ["selling-products"],
+    queryFn: () => fetcher(`/api/ingredients`),
+  });
+
+  const mutation = useMutation({
+    mutationKey: ["update-bundle-product"],
+    mutationFn: (values: any) => axios.put(`/api/bundle/product`, values),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["selling-products"]);
+      setOpen(false);
+    },
+  });
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                <div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-base font-semibold leading-6 text-gray-900"
+                    >
+                      Modifier le produit
+                    </Dialog.Title>
+                    <Formik
+                      initialValues={{
+                        name: product.name,
+                        price: product.price,
+                        ingredients: product.ingredients,
+                      }}
+                      onSubmit={(values) => {
+                        mutation.mutate({
+                          ...values,
+                          productId: product.id,
+                        });
+                        setOpen(false);
+                      }}
+                      onReset={() => {
+                        setOpen(false);
+                        mutation.reset();
+                      }}
+                    >
+                      {({ values }) => (
+                        <Form className="mt-2">
+                          <div className="text-sm text-gray-500">
+                            <div>
+                              <label>NOM DU PRODUIT</label>
+                              <Field
+                                name={"name"}
+                                type="text"
+                                className="border border-gray-300 rounded-md w-full"
+                              />
+                            </div>
+                            <div>
+                              <label>PRIX DE VENTE</label>
+                              <Field
+                                name={"price"}
+                                type="text"
+                                className="border border-gray-300 rounded-md w-full"
+                              />
+                            </div>
+                            <div>
+                              <label>INGREDIENTS</label>
+
+                              <FieldArray
+                                name={"ingredients"}
+                                render={(arrayHelpers) => (
+                                  <>
+                                    <select
+                                      onChange={(e) => {
+                                        const _ingredient = ingredients.find(
+                                          (ingredient) =>
+                                            ingredient.name === e.target.value
+                                        );
+                                        if (
+                                          !values.ingredients.find(
+                                            (ingredient) =>
+                                              ingredient.id === _ingredient.id
+                                          )
+                                        ) {
+                                          arrayHelpers.push({
+                                            name: _ingredient.name,
+                                            qty: 1,
+                                            unit: _ingredient.sellingPerUnit
+                                              .unit,
+                                            id: _ingredient.id,
+                                          });
+                                        }
+                                      }}
+                                      className={
+                                        "w-full border border-gray-300 p-2"
+                                      }
+                                    >
+                                      {ingredients?.map((ingredient) => (
+                                        <option
+                                          key={ingredient.id}
+                                          value={ingredient.name}
+                                        >
+                                          {ingredient.name}
+                                        </option>
+                                      ))}
+                                    </select>
+                                    <div className={"h-72 overflow-auto"}>
+                                      {values.ingredients.map(
+                                        (ingredient, index) => {
+                                          return (
+                                            <div
+                                              key={index}
+                                              className={
+                                                "flex items-center justify-between p-3 border space-x-8"
+                                              }
+                                            >
+                                              <div
+                                                className={
+                                                  "flex items-center justify-center space-x-3"
+                                                }
+                                              >
+                                                <span
+                                                  className={
+                                                    "first-letter:uppercase"
+                                                  }
+                                                >
+                                                  {ingredient.name}
+                                                </span>
+                                                <span>{ingredient.qty}</span>
+                                              </div>
+                                              <div className={"space-x-3"}>
+                                                <button
+                                                  type={"button"}
+                                                  className={
+                                                    "border rounded-md px-4 py-2"
+                                                  }
+                                                  onClick={() => {
+                                                    const product =
+                                                      values.ingredients.find(
+                                                        (_ingredient) =>
+                                                          _ingredient.id ===
+                                                          ingredient.id
+                                                      );
+
+                                                    product.qty++;
+                                                    arrayHelpers.replace(
+                                                      index,
+                                                      product
+                                                    );
+                                                  }}
+                                                >
+                                                  +
+                                                </button>
+                                                <button
+                                                  type={"button"}
+                                                  className={
+                                                    "border rounded-md px-4 py-2"
+                                                  }
+                                                  onClick={() => {
+                                                    const product =
+                                                      values.ingredients.find(
+                                                        (_ingredient) =>
+                                                          _ingredient.id ===
+                                                          ingredient.id
+                                                      );
+                                                    if (product.qty)
+                                                      product.qty--;
+                                                    arrayHelpers.replace(
+                                                      index,
+                                                      product
+                                                    );
+                                                  }}
+                                                >
+                                                  -
+                                                </button>
+                                                <button
+                                                  type={"button"}
+                                                  className={
+                                                    "rounded-md px-4 py-2 border"
+                                                  }
+                                                  onClick={() => {
+                                                    arrayHelpers.remove(index);
+                                                  }}
+                                                >
+                                                  <TrashIcon
+                                                    className={"w-5 h-5"}
+                                                  />
+                                                </button>
+                                              </div>
+                                            </div>
+                                          );
+                                        }
+                                      )}
+                                    </div>
+                                  </>
+                                )}
+                              />
+                            </div>
+                          </div>
+                          <div className="mt-5 sm:mt-6">
+                            {mutation.isIdle && (
+                              <button
+                                type="submit"
+                                className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                onClick={() => setOpen(false)}
+                              >
+                                Valider
+                              </button>
+                            )}
+                            {mutation.isLoading && (
+                              <div
+                                className={
+                                  "rounded-full p-2 border border-t-blue-500 w-5 h-5 animate-spin"
+                                }
+                              />
+                            )}
+                            {mutation.isSuccess && (
+                              <button
+                                onClick={() => {
+                                  setOpen(false);
+                                  mutation.reset();
+                                }}
+                                type="reset"
+                                className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                              >
+                                Modification effectuée!
+                              </button>
+                            )}
+                          </div>
+                        </Form>
+                      )}
+                    </Formik>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
---
+++ pages/api/bundle/product.ts
@@ -0,0 +1,105 @@
+// Nextjs handler
+import { ObjectId } from "mongodb";
+import clientPromise from "@/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    try {
+      const client = await clientPromise;
+      const {
+        name, //productName
+        categoryId, // categoryId
+        cookingPlace, // cookingPlace
+        buyingPrice,
+        sellingPrice,
+        margin,
+        stands, // point of sales
+        ingredients,
+      } = req.body;
+
+      if (
+        !name ||
+        !categoryId ||
+        !cookingPlace ||
+        !buyingPrice ||
+        !sellingPrice ||
+        !stands ||
+        !ingredients
+      ) {
+        return res.status(400).send({
+          msg: "Bad request",
+        });
+      }
+
+      const db = client.db();
+      //Check if product already exists
+      const product = await client.db().collection("sellingProducts").findOne({
+        name: name.toLowerCase(),
+      });
+
+      if (product) {
+        return res.status(400).send({
+          msg: "Produit déjà existant!",
+        });
+      }
+
+      for (const standId of stands) {
+        const productId = new ObjectId();
+        await db.collection("sellingProducts").insertOne({
+          _id: productId,
+          id: productId.toString(),
+          standId,
+          categoryId,
+          name: name.toLowerCase(),
+          price: sellingPrice,
+          deStock: false,
+          special: false,
+          bundle: true,
+          cookingPlace,
+          buyingPrice,
+          margin,
+          ingredients,
+        });
+      }
+
+      res.send({
+        msg: "Product added successfully",
+      });
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).send({
+        msg: error.message,
+      });
+    }
+  }
+
+  if (req.method === "PUT") {
+    try {
+      const { ingredients, name, price, productId } = req.body;
+      console.log(req.body);
+      const payload = {
+        ...(ingredients && { ingredients }),
+        ...(name && { name }),
+        ...(price && { price }),
+      };
+      const client = await clientPromise;
+      await client.db().collection("sellingProducts").updateOne(
+        {
+          id: productId,
+        },
+        {
+          $set: payload,
+        }
+      );
+
+      res.send({ msg: "Updated!" });
+    } catch (error: any) {
+      console.log(error);
+      res.status(500).send({ msg: "Serveur Error" });
+    }
+  }
+}
